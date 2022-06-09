401ac8e09cba190769113f9bd5a0d56f80adb890
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2022-06-09 10:44:18 +0530

    guid tour changes

--- assets/lib/guide-tour/guide-tour-data.js
+++ assets/lib/guide-tour/guide-tour-data.js
@@ -1,4 +1,4 @@
-const g_tour_steps = [
+/*const g_tour_steps = [
     {
         sourceElmIdQS: "#btn_info",
         informationText: `Instructions - Describes the different steps of experiment.`,
@@ -8,3 +8,36 @@ const g_tour_steps = [
         informationText: `Theme - Provides different user interface colour themes.`,
     }
 ]
+*/
+
+const g_tour_steps = [
+    {
+        sourceElmIdQS: "#btn_info",
+        informationText: "Instructions - Describes the different steps of experiment.",
+    },
+    {
+        sourceElmIdQS: "#btn_reset",
+        informationText: `Reset - Reset the exercise with new set of values.`,
+    },
+    {
+        sourceElmIdQS: "#OK_btn",
+        informationText: `Ok - Evaluate the Algebraic expressions and display feedback`,
+    },
+    {
+        sourceElmIdQS: "#explain_btn",
+        informationText: `Explain - Explain the evaluation of Algebraic expression.`,
+    },
+    {
+        sourceElmIdQS: "#next_btn",
+        informationText: `Next - Display next question.`,
+    },
+    
+    {
+        sourceElmIdQS: ".gutter .gutter_handle",
+        informationText: `Slider - Slide to adjust view area.`,
+    },
+    {
+        sourceElmIdQS: "#btn_themes",
+        informationText: "Theme - Provides different user interface colour themes.",
+    },
+];
